3a86099899be97c560f85aba8235f40e7d474f8a
Raphael <rapha4304@gmail.com>
2025-11-09 13:17:27 -0300

    1 - somar 2 numeros

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 a = int(input("Digite um número: "))
-b = int(input("Digite outro número:"))
+b = int(input("Digite outro número: "))
 r = a + b
-print(r)
-print("Resuldado")
+
+print(f"Resuldado é: {r}")
