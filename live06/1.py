dcbd1e90a8350489bec75fa62a93b97f73022c67
Raphael <rapha4304@gmail.com>
2025-11-09 13:56:04 -0300

    separar por pastas

--- live06/1.py
+++ live06/1.py
@@ -1,3 +1,6 @@
+# Input foi utilizado para capturar a entrada do usuário
+# int() converte a entrada do usuário (que é uma string) em um número inteiro
+
 a = int(input("Digite um número: "))
 b = int(input("Digite outro número: "))
 r = a + b
